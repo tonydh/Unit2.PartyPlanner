74bcf5bd76cb439eeb8806b0ebf259f33be2d35a
tonydh <tonydigitalart@gmail.com>
2023-12-05 03:36:04 -0800

    updating functions

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const COHORT = `2310-FSA-ET-WEB-PT-SF`;
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
+const COHORT = "2310-FSA-ET-WEB-PT-SF";
+const API_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events";
 
 const state = {
     events: [],
 }
 
-const addEventForm = document.querySelector(`#addEvent`);
+const addEventForm = document.querySelector("#addEvent");
+
+addEventForm.addEventListener("submit", addEvent);
+
 
 /**
  * sync state with the API and re-render
@@ -21,7 +24,7 @@ render();
  * update state with events from API
  */
 
-async function getEvents(){
+async function getEvents() {
     try {
         const response = await fetch(API_URL);
         const json = await response.json();
@@ -32,6 +35,40 @@ async function getEvents(){
     }
 }
 
+async function addEvent(event) {
+    try {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({
+                name: addEventForm.name.value,
+                description: addEventForm.description.value,
+                date: addEventForm.date.value,
+                location: addEventForm.location.value
+            }),
+        })
+
+        if(!response.ok) {
+            throw new Error("Failed to add new event.");
+        }
+
+        render();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function deleteEvent(id) {
+    try {
+        await fetch(API_URL + `/${id}`, {
+            method: "DELETE",
+        });
+        render();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 function renderEvents() {
     if(!state.events.length) {
         eventContainer.innerHTML = "No events, please add one.";
@@ -39,16 +76,17 @@ function renderEvents() {
     }
 
     const eventInfo = state.events.map((event) => {
-        const listAllEvents = document.createElement(`li`);
+        const listAllEvents = document.createElement("li");
         listAllEvents.innerHTML = `
-            <h3>${event.name}</h3>
+            <h2>${event.name}</h2>
             <h4>${event.date}</h4>
             <h4>${event.location}</h4>
             <p>${event.description}</p>
+            <button onclick="deleteEvent(${event.id})">Delete Event</button>
         `;
 
         return listAllEvents;
     })
     
-    eventContainer.replaceChildren(...eventInfo)
-}
+    eventContainer.replaceChildren(...eventInfo);
+};
